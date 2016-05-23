318e1d8af16511ee9f98aee6c4b491f1d55503a4
Joshua Coats <joshu@fearchar.net>
2016-05-22 20:47:53 -0700

    Switching away from ioutil.ReadAll() per feedback

--- client.go
+++ client.go
@@ -3,20 +3,18 @@ package humphrey
 import (
 	"encoding/xml"
 	"fmt"
-	"io/ioutil"
 	"net/http"
 	"net/url"
 )
 
 // A Client lets you use the default BART client or your own API Key
 type Client struct {
-	apiKey string
+	APIKey string
 }
 
 // MakeRequest is the heart of the client library
 func (c *Client) MakeRequest(path string, cmd string, params url.Values, v interface{}) error {
-	params["key"] = []string{c.apiKey}
-	params["cmd"] = []string{cmd}
+	params["key"], params["cmd"] = []string{c.APIKey}, []string{cmd}
 	requestURL := url.URL{Scheme: "http", Host: "api.bart.gov", Path: fmt.Sprintf("/api/%s.aspx", path), RawQuery: params.Encode()}
 	response, err := http.Get(requestURL.String())
 	if err != nil {
@@ -25,13 +23,11 @@ func (c *Client) MakeRequest(path string, cmd string, params url.Values, v inter
 	if response.StatusCode != 200 {
 		return fmt.Errorf("received non-200 status code (%d) requesting \"%s\"", response.StatusCode, requestURL.String())
 	}
-	responseBody, err := ioutil.ReadAll(response.Body)
+	defer response.Body.Close()
+	xmlDecoder := xml.NewDecoder(response.Body)
+	err = xmlDecoder.Decode(v)
 	if err != nil {
-		return fmt.Errorf("reading the body of the response: %v", err)
-	}
-	err = xml.Unmarshal(responseBody, v)
-	if err != nil {
-		return fmt.Errorf("unmarshalling the response: %v", err)
+		return fmt.Errorf("decoding the response: %v", err)
 	}
 	return nil
 }
